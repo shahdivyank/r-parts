520cf634b43a42b2e77a6447b5c9ae47542a0110
Divyank Shah <divyank.shah.2016@gmail.com>
2023-03-27 23:25:16 -0700

    eslint setup

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "../styles/Home.module.css";
 
+// eslint-disable-next-line new-cap
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
