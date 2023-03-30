88ad35fc9548005b2985a1c1d943cad3b3dc8d9d
Rodrigo <rddlamas@gmail.com>
2023-03-30 16:49:56 -0700

    created layout format to include the footer and eventually navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  );
 }
