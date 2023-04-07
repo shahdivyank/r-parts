d0062901503488d5bf53ce8db21dc2b95cb7eba6
msclal <mika1shanela@gmail.com>
2023-04-07 14:18:41 -0700

    updated Tailwind colors again

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,15 @@ module.exports = {
     extend: {
       colors: {
         rparts: {
-          darkGray: "#393B41",
-          darkBlue: "#123666",
+          black: "#000000",
+          footerBlue: "#123666",
           blue: "#2D6CC0",
-          lightBlue: "#329AF0",
           orange: "#FF8F28",
-          yellow: "#F1AB00",
           white: "#F5F5F5",
+          wrappergGray: "#F0F0F0",
+          bannerGray: "#E6E6E6",
+          borderGray: "#D9D9D9",
+          subHeadingtGray: "#858585",
         },
       },
     },
