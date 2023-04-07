c782163e5028797bdb5dbf9c557313f1d972cd86
msclal <mika1shanela@gmail.com>
2023-04-07 05:31:35 -0700

    Add Tailwind Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rparts: {
+          darkGray: "#393B41",
+          darkBlue: "#123666",
+          blue: "#2D6CC0",
+          lightBlue: "#329AF0",
+          orange: "#FF8F28",
+          yellow: "#F1AB00",
+          white: "#F5F5F5",
+        },
+      },
+    },
   },
   plugins: [],
 };
