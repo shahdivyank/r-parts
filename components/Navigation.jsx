3e6452d645bb0c0bcbeaf9a40f15289d123f41a2
msclal <mika1shanela@gmail.com>
2023-04-07 17:15:34 -0700

    💅added font styling to navbar

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -8,21 +8,26 @@ export default function Navigation() {
   return (
     <div className="flex justify-between items-center my-3 mx-12">
       <div className="flex w-1/3 justify-around">
-        <span className="flex items-center text-2xl font-medium gap-x-2">
-          <BsGear className="stroke-0" /> {"R'Parts"}
+        <span className="flex items-center text-2xl font-medium gap-x-2 font-montserrat">
+          <BsGear className="stroke-0" />
+          <div className="flex">
+            <p>{"R'P"}</p>
+            <p className="italic">A</p>
+            <p>RTS</p>
+          </div>
         </span>
-        <div className="w-2/3 flex items-center justify-between border-[1px] border-[#D9D9D9] px-4 py-2 rounded-3xl">
-          <p className="text-[#858585]">Search {"R'Parts"}</p>
-          <p className="text-[#858585]">
+        <div className="w-2/3 flex items-center justify-between border-[1px] border-borderGray px-4 py-2 rounded-3xl">
+          <p className="text-subHeadingtGray">Search {"R'Parts"}</p>
+          <p className="text-subHeadingtGray">
             <RiSearchLine />
           </p>
         </div>
       </div>
       <div className="flex w-1/6 justify-between">
         <div className="flex gap-x-6">
-          <p className="font-medium">Market</p>
-          <p className="font-medium">Sell</p>
-          <p className="font-medium">Info</p>
+          <p className="font-outfit font-medium">Market</p>
+          <p className="font-outfit font-medium">Sell</p>
+          <p className="font-outfit font-medium">Info</p>
         </div>
         <span className="border-[0.5px] border-black px-0 mx-0"></span>
         <div className="flex gap-x-6">
