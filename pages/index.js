4c1f2b6e893fa72dce65fc319ec57040da75288a
msclal <mika1shanela@gmail.com>
2023-04-08 00:40:11 -0700

    added mika pic

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Hello from "../components/Hello";
 import CartItems from "../components/CartItems";
+import ProfileImage from "../components/ProfileImage";
 
 export default function Home() {
   return (
     <>
       <Hello />
-      <CartItems />
+      <ProfileImage />
     </>
   );
 }
