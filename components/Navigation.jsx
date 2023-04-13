a1f936d1eea393c34074780117bba39032e31c2f
msclal <mika1shanela@gmail.com>
2023-04-13 01:02:11 -0700

    added nav bar routing links

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -3,22 +3,23 @@ import { BsGear } from "react-icons/bs";
 import { FaRegUser } from "react-icons/fa";
 import { RiSearchLine } from "react-icons/ri";
 import { HiOutlineShoppingBag } from "react-icons/hi";
+import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <div className="flex justify-between items-center my-3 mx-12">
+    <div className="flex justify-between items-center my-3 mx-12 font-outfit">
       <div className="flex w-1/3 justify-around">
-        <span className="flex items-center text-2xl font-medium gap-x-2 font-montserrat">
+        <button className="flex items-center text-2xl font-medium gap-x-2 font-montserrat">
           <BsGear className="stroke-[0.5px] rotate-12" />
           <div className="flex">
             <p>{"R'P"}</p>
             <p className="italic">A</p>
             <p>RTS</p>
           </div>
-        </span>
+        </button>
         <div className="w-2/3 flex items-center justify-between border-[1px] border-rparts-borderGray px-4 py-2 rounded-3xl">
           <input
-            className="w-full font-outfit focus:outline-none"
+            className="w-full focus:outline-none"
             type="search"
             placeholder={"Search R'Parts"}
           />
@@ -26,16 +27,37 @@ export default function Navigation() {
         </div>
       </div>
       <div className="flex gap-x-10 justify-between">
-        <p className="font-outfit font-medium">MARKET</p>
-        <p className="font-outfit font-medium">SELL</p>
-        <p className="font-outfit font-medium">INFO</p>
-        <span className="border-[0.5px] border-black px-0 mx-0"></span>
-        <span className="flex items-center">
+        <Link
+          href="/"
+          className="flex items-center hover:text-rparts-subHeadingGray font-medium"
+        >
+          MARKET
+        </Link>
+        <Link
+          href="/"
+          className="flex items-center hover:text-rparts-subHeadingGray font-medium"
+        >
+          SELL
+        </Link>
+        <Link
+          href="/"
+          className="flex items-center hover:text-rparts-subHeadingGray font-medium"
+        >
+          INFO
+        </Link>
+        <span className="border-[0.5px] border-black px-0 mx-0" />
+        <Link
+          href="/profile"
+          className="flex items-center hover:text-rparts-subHeadingGray"
+        >
           <FaRegUser className="flex items-center stroke-2 text-xl" />
-        </span>
-        <span className="flex items-center">
+        </Link>
+        <Link
+          href="/"
+          className="flex items-center hover:text-rparts-subHeadingGray"
+        >
           <HiOutlineShoppingBag className="flex items-center stroke-2 text-2xl" />
-        </span>
+        </Link>
       </div>
     </div>
   );
