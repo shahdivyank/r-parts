8dfd451903191ec46d7d0028673797e5132fc0f9
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-04-13 00:08:54 -0700

    prettier

--- components/Landing4.jsx
+++ components/Landing4.jsx
@@ -5,12 +5,46 @@ const Landing4 = () => {
   return (
     <>
       <div>
-        <div className="relative flex justify-between">
-          <div>Sustainability</div>
-          <div>Community</div>
-        </div>
-        <div className="absolute w-1/3 flex justify-center bg-red-500">
-          <Image src={Landing4SVG} alt="Landing" layout="responsive" />
+        <div className="relative mx-64 flex flex-col space-y-10 mb-20">
+          <div className="flex justify-between bg-rparts-white p-5 rounded-3xl">
+            <div>
+              <div className="font-bebasNeue text-7xl">SUSTAINABILITY</div>
+              <div className="font-outfit font-light w-1/2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum convallis mi ut velit porttitor placerat. Nulla
+                egestas magna.
+              </div>
+            </div>
+            <div className="flex items-end flex-col">
+              <div className="font-bebasNeue text-7xl">COMMUNITY</div>
+              <div className="font-outfit font-light text-right w-1/2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum convallis mi ut velit porttitor placerat. Nulla
+                egestas magna.
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between bg-rparts-white p-5 rounded-3xl">
+            <div>
+              <div className="font-bebasNeue text-7xl">INNOVATION</div>
+              <div className="font-outfit font-light w-1/2 ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum convallis mi ut velit porttitor placerat. Nulla
+                egestas magna.
+              </div>
+            </div>
+            <div className="flex items-end flex-col">
+              <div className="font-bebasNeue text-7xl">GROWTH</div>
+              <div className="font-outfit font-light text-right w-1/2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum convallis mi ut velit porttitor placerat. Nulla
+                egestas magna.
+              </div>
+            </div>
+            <div className="absolute w-1/2 left-[25%] -top-[8%]">
+              <Image src={Landing4SVG} alt="Landing" layout="responsive" />
+            </div>
+          </div>
         </div>
       </div>
     </>
