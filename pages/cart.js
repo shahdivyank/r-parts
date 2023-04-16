a244a68bf8deb6c2b6feb174063fc3d7ae9d70f5
msclal <mika1shanela@gmail.com>
2023-04-16 00:22:14 -0700

    updated margins and font boldness

--- pages/cart.js
+++ pages/cart.js
@@ -4,20 +4,20 @@ import CartTotal from "../components/CartTotal";
 export default function Cart() {
   return (
     <section className="h-full w-screen font-outfit pt-4 pb-20">
-      <div className="flex justify-evenly">
-        <div className="w-1/2 flex flex-col justify-center">
+      <div className="flex justify-center gap-x-20">
+        <div className="w-[40%] flex flex-col justify-center">
           <div className="flex gap-x-4 font-thin text-rparts-subheadingGray text-xs mb-6">
             <button>MARKET</button>
             <p>/</p>
             <button>SHOPPING CART</button>
           </div>
           <div className="font-bebasNeue text-3xl">MY SHOPPING BAG</div>
-          <div className="text-sm font-thin text-rparts-subheadingGray mb-4">
+          <div className="text-sm text-rparts-subheadingGray mb-4">
             View current items in cart and finalize before checkout
           </div>
           <CartItems />
         </div>
-        <div className="w-1/4 mt-32">
+        <div className="w-1/4 mt-[7.5rem]">
           <CartTotal />
         </div>
       </div>
