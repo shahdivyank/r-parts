99a5c6441c09121c0b46fbd5c9cf4da082b242dd
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-04-18 18:49:16 -0700

    ran prettier

--- pages/sell.js
+++ pages/sell.js
@@ -1,13 +1,46 @@
+import SellPhoto from "../components/SellPhoto";
+import AddPhoto from "../components/AddPhoto";
+
 export default function Sell() {
   return (
     <>
-      <div className="font-outfit pt-4 pb-20">
-        <div className="flex justify-start">
-          <div className="">
-            <div className="font-bebasNeue text-3xl">LIST YOUR PART</div>
-            <div className="text-sm text-rparts-subheadingGray mb-4 font-light">
-              Fill out the following prompts that accurately describe your part
+      <div className="mx-48">
+        <div className="font-outfit w-fit pt-4">
+          <div className="font-bebasNeue text-3xl">LIST YOUR PART</div>
+          <div className="text-sm text-rparts-subheadingGray mb-4 font-light">
+            Fill out the following prompts that accurately describe your part
+          </div>
+        </div>
+        <div className="flex space-x-6">
+          <SellPhoto />
+          <AddPhoto />
+        </div>
+        <input
+          type="text"
+          className="font-outfit resize-none text-3xl w-full px-3 py-3 border-b mt-5 mb-10 placeholder-rparts-subheadingGray"
+          placeholder="Enter Name of Product Here"
+        />
+
+        <div className="flex">
+          <div>
+            <div className="text-xl font-outfit font-medium">
+              Proposed Price
             </div>
+            <input
+              type="text"
+              className=" border rounded-full px-3 py-3 placeholder:font-outfit"
+              placeholder="Enter your proposed price here"
+            />
+          </div>
+          <div>
+            <div className="text-xl font-outfit font-medium">
+              Proposed Price
+            </div>
+            <input
+              type="text"
+              className="border rounded-full px-3 py-3 placeholder:font-outfit"
+              placeholder="Enter your proposed price here"
+            />
           </div>
         </div>
       </div>
