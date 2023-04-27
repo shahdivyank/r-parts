bf278ef8ebdea4aa2a7c32e0cb747016bffaac81
Zorten <zergionaruto@hotmail.com>
2023-04-26 23:26:26 -0700

    Made info banner its own svg

--- pages/info.js
+++ pages/info.js
@@ -1,10 +1,9 @@
-import InfoBanner from "../public/InfoBanner.svg";
-import Image from "next/image";
+import InfoBanner from "../components/SVGs/InfoBanner";
 
 export default function Info() {
   return (
     <div className="bg-rparts-white flex justify-center items-center">
-      <Image src={InfoBanner} alt="INFO BANNER" responsive />
+      <InfoBanner />
     </div>
   );
 }
