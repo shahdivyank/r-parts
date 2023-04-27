28103decaec3d6b5d306312e2d7c1efb85cf765c
Zorten <zergionaruto@hotmail.com>
2023-04-27 00:21:46 -0700

    Added what is RParts component

---
+++ components/WhatIsRParts.jsx
@@ -0,0 +1,45 @@
+const WhatIsRParts = () => {
+  return (
+    <section id="introduction">
+      <div className="bg-white rounded-3xl font-outfit border-2 mt-16 mb-28 pb-8">
+        <div>
+          <p className="m-0 text-2xl my-3 pl-8 pt-8 border-b-2">
+            What Is RParts
+          </p>
+          <div className="px-8 ml-8">
+            <p className="text-md">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ullamcorper morbi tincidunt ornare massa eget egestas purus. Justo
+              laoreet sit amet cursus sit. Sed nisi lacus sed viverra tellus in
+              hac habitasse. Nunc mi ipsum faucibus vitae aliquet nec
+              ullamcorper sit. Praesent tristique magna sit amet purus.
+              Adipiscing elit pellentesque habitant morbi tristique. Vitae
+              sapien pellentesque habitant morbi tristique senectus et netus et.
+              Varius vel pharetra vel turpis nunc eget lorem dolor sed. Sit amet
+              cursus sit amet dictum sit. Turpis massa tincidunt dui ut ornare
+              lectus sit amet est. In nisl nisi scelerisque eu ultrices.
+              Curabitur vitae nunc sed velit dignissim sodales ut eu.
+            </p>
+            <p className="text-md">
+              Sit amet consectetur adipiscing elit pellentesque habitant morbi.
+              Non arcu risus quis varius quam quisque id. Quis auctor elit sed
+              vulputate mi sit amet mauris. Placerat duis ultricies lacus sed
+              turpis tincidunt id aliquet risus. Et odio pellentesque diam
+              volutpat commodo sed egestas egestas. Nunc pulvinar sapien et
+              ligula ullamcorper malesuada. Tempor orci dapibus ultrices in
+              iaculis nunc sed augue. Tincidunt ornare massa eget egestas purus
+              viverra accumsan. Luctus venenatis lectus magna fringilla urna. At
+              elementum eu facilisis sed odio morbi quis. Risus nullam eget
+              felis eget nunc lobortis. Lectus urna duis convallis convallis
+              tellus id interdum velit laoreet. Nisl pretium fusce id velit ut
+              tortor pretium viverra suspendisse.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhatIsRParts;
--- pages/info.js
+++ pages/info.js
@@ -1,9 +1,21 @@
 import InfoBanner from "../components/SVGs/InfoBanner";
+import WhatIsRParts from "../components/WhatIsRParts";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Info() {
   return (
-    <div className="bg-rparts-white flex justify-center items-center">
+    <div className="bg-rparts-white flex justify-center items-center flex-col pt-0">
       <InfoBanner />
+
+      <Row className="flex justify-center items-start w-10/12 ">
+        <Col
+          xl={8}
+          className="m-0 p-0 flex justify-center items-start flex-col"
+        >
+          <WhatIsRParts />
+        </Col>
+      </Row>
     </div>
   );
 }
