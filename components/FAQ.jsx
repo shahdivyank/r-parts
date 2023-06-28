0e6f97a8ae781dc8dcfa93cd6850b39897cdaea7
Zorten <zergionaruto@hotmail.com>
2023-06-27 17:50:22 -0700

    Began FAQ component

---
+++ components/FAQ.jsx
@@ -0,0 +1,30 @@
+const FAQ = () => {
+  return (
+    <section id="faqs">
+      <div className="bg-white rounded-3xl font-outfit border-2 mt-16 mb-28 pb-8">
+        <div>
+          <p className="text-2xl my-3 pt-2 ml-8 w-5/6 border-b-2 pb-3 font-medium">
+            FAQs
+          </p>
+          <div className="px-4 ml-4">
+            <button class="bg-transparent font-semibold py-2 px-4 border hover:border-transparent rounded">
+              Button
+            </button>
+            <p className="text-md pt-2">STEP 1:</p>
+            <p className="text-md pb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ullamcorper morbi tincidunt ornare massa eget egestas purus. Justo
+              laoreet sit amet cursus sit. Sed nisi lacus sed viverra tellus in
+              hac habitasse. Nunc mi ipsum faucibus vitae aliquet nec
+              ullamcorper sit.
+              <br />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FAQ;
