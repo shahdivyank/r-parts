c60e5546189f2b2cca82c0aa14efbea0e9229344
Zorten <zergionaruto@hotmail.com>
2023-07-04 15:47:33 -0700

    Created FAQ component. Having trouble with the initial width, don't know how to set it to be as wide as the figma shows

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -1,25 +1,67 @@
+import Accordion from "react-bootstrap/Accordion";
+
 const FAQ = () => {
   return (
     <section id="faqs">
-      <div className="bg-white rounded-3xl font-outfit border-2 mt-16 mb-28 pb-8">
-        <div>
-          <p className="text-2xl my-3 pt-2 ml-8 w-5/6 border-b-2 pb-3 font-medium">
-            FAQs
-          </p>
-          <div className="px-4 ml-4">
-            <button class="bg-transparent font-semibold py-2 px-4 border hover:border-transparent rounded">
-              Button
-            </button>
-            <p className="text-md pt-2">STEP 1:</p>
-            <p className="text-md pb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ullamcorper morbi tincidunt ornare massa eget egestas purus. Justo
-              laoreet sit amet cursus sit. Sed nisi lacus sed viverra tellus in
-              hac habitasse. Nunc mi ipsum faucibus vitae aliquet nec
-              ullamcorper sit.
-              <br />
+      <div className="flex mb-4">
+        <div className="w-full">
+          <div className="bg-white rounded-3xl font-outfit border-2 mt-16 mb-28 pb-8">
+            <p className="text-2xl my-3 pt-2 ml-8 w-5/6 border-b-2 pb-3 font-medium">
+              FAQs
             </p>
+            <div className="mx-7">
+              <Accordion>
+                <div className="pb-3">
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>
+                      Where does the money go?
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </div>
+                <div className="pb-3">
+                  <Accordion.Item eventKey="1">
+                    <Accordion.Header>
+                      Where does the money go?
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </div>
+
+                <div className="pb-3">
+                  <Accordion.Item eventKey="2">
+                    <Accordion.Header>
+                      Where does the money go?
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </div>
+                <div className="pb-3">
+                  <Accordion.Item eventKey="3">
+                    <Accordion.Header>
+                      Where does the money go?
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </div>
+              </Accordion>
+            </div>
           </div>
         </div>
       </div>
