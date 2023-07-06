cfd3812c6bb5daf53edf9412736d8c0fecb6fa28
Zorten <zergionaruto@hotmail.com>
2023-07-06 10:53:38 -0700

    Began creating Nav. Made one skip link that redirects to FAQ component. Need to fix its placing, sizing, and design.

--- pages/info.js
+++ pages/info.js
@@ -9,7 +9,14 @@ export default function Info() {
   return (
     <div className="bg-rparts-white flex justify-center items-center flex-col pt-0">
       <InfoBanner />
-
+      <a
+        href="#main-content"
+        class="absolute px-3 py-2 transition-all -translate-y-full focus:translate-y-0 bg-indigo-800 text-gray-100 text-base font-medium rounded-b-lg mx-4 focus:outline-none focus:ring-4 focus:ring-indigo-500  hover:no-underline hover:bg-indigo-900 hover:text-white z-10"
+      >
+        {" "}
+        FAQ
+      </a>
+      <div class="flex items-center justify-center min-h-screen from-teal-100 via-teal-300 to-teal-500 bg-gradient-to-br"></div>
       <Row className="flex justify-center items-start w-10/12 ">
         <Col
           xl={8}
@@ -17,7 +24,9 @@ export default function Info() {
         >
           <WhatIsRParts />
           <HowItWorks />
-          <FAQ />
+          <main id="main-content">
+            <FAQ />
+          </main>
         </Col>
       </Row>
     </div>
