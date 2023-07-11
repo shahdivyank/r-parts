83d8005e9832546f1b7512c12d7db2b046cc9c46
Zorten <zergionaruto@hotmail.com>
2023-07-11 12:02:07 -0700

    Added images

--- components/InfoNav.jsx
+++ components/InfoNav.jsx
@@ -1,3 +1,8 @@
+import Image from "next/image";
+import gearNav from "../public/gear_info_nav.png";
+import arrow from "../public/arrow_info_nav.png";
+import faqNav from "../public/faq_info_nav.png";
+
 const InfoNav = () => {
   return (
     <section id="info-nav">
@@ -6,16 +11,16 @@ const InfoNav = () => {
           href="#what-is"
           className="font-outfit mb-2 px-3 py-2 transition-all bg-black text-rparts-subheadingGray rounded-r-full mr-24 hover:bg-rparts-hoverOrange hover:text-white w-52 h-12"
         >
-          {" "}
-          What is R’Parts
+          <Image src={gearNav} alt="acm logo" width={24} height={24} /> What is
+          R’Parts
         </a>
 
         <a
           href="#how-it-works"
           className="font-outfit mb-2 px-3 py-2 transition-all bg-black text-rparts-subheadingGray rounded-r-full mr-24 hover:bg-rparts-orange hover:text-white w-52 h-12"
         >
-          {" "}
-          How It Works
+          <Image src={arrow} alt="acm logo" width={24} height={24} /> How It
+          Works
         </a>
 
         <div className="flex bg-white ">
@@ -23,8 +28,7 @@ const InfoNav = () => {
             href="#faqs"
             className="font-outfit px-3 py-2 transition-all bg-black text-rparts-subheadingGray rounded-r-full mr-24 hover:bg-rparts-orange hover:text-white w-52 h-12"
           >
-            {" "}
-            FAQ
+            <Image src={faqNav} alt="acm logo" width={24} height={24} /> FAQ
           </a>
         </div>
       </div>
