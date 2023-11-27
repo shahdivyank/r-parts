d326d3b28b1baddff0b119031fda4659b1551145
menthy-wu <wumengxuan0728@gmail.com>
2023-11-27 13:17:18 -0800

    add description section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,13 @@
-import { Inter } from "next/font/google";
+// eslint-disable-next-line camelcase
+import { Bebas_Neue } from "next/font/google";
 import "./globals.css";
 
 // eslint-disable-next-line new-cap
-const inter = Inter({ subsets: ["latin"] });
+const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bebas",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${bebas.variable}`}>{children}</body>
     </html>
   );
 }
