aa0ae47a073f700297de2cc9c4d508e35ed1029d
Alamode89 <andreamante89@gmail.com>
2023-11-26 22:35:42 -0800

    Added figma colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        parts: {
+          black: "#000000",
+          navy: "#123666",
+          sky: "#329AF0",
+          orange: "#FF8F28",
+          green: "#8CBF83",
+          "gray-100": "#F5F5F5",
+          "gray-200": "#F0F0F0",
+          "gray-300": "#E6E6E6",
+          "gray-400": "#D9D9D9",
+          "gray-500": "#858585",
+        },
+      },
     },
   },
   plugins: [],
