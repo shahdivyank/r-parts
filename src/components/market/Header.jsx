9c633e65e2737b1ec4aa8dc093a971ea76505d67
menthy-wu <wumengxuan0728@gmail.com>
2023-12-26 21:43:44 -0800

    better naming

--- src/components/market/Header.jsx
+++ src/components/market/Header.jsx
@@ -1,13 +1,15 @@
 const Header = ({ text }) => {
   return (
     <div className="w-full overflow-hidden flex flex-col items-end justify-center h-[80px] md:h-[120px] lg:h-[190px]">
-      {Array.from({ length: 3 }).map((_, i) => (
-        <div key={i} className="text-4xl md:text-6xl lg:text-8xl font-bebas">
-          {Array.from({ length: 10 }, (_, j) => (
+      {Array.from({ length: 3 }).map((_, row) => (
+        <div key={row} className="text-4xl md:text-6xl lg:text-8xl font-bebas">
+          {Array.from({ length: 10 }, (_, col) => (
             <span
-              key={j}
+              key={col}
               className={`${
-                i === 1 && j === 9 ? "text-parts-orange" : "text-parts-gray-200"
+                row === 1 && col === 9
+                  ? "text-parts-orange"
+                  : "text-parts-gray-200"
               }`}
             >
               {text}
