07b81dfde80bd83567c27b06d229feda12dad4ab
menthy-wu <wumengxuan0728@gmail.com>
2023-12-28 12:00:49 -0800

    add hover

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import { COLOR, ROUNDED, SIZE } from "@/data/Button";
 const Button = ({ text, onClick, rounded, color, size }) => {
   return (
     <div
-      className={`cursor-pointer ${COLOR[color]} ${ROUNDED[rounded]} ${SIZE[size]}`}
+      className={`cursor-pointer hover:opacity-90 ${COLOR[color]} ${ROUNDED[rounded]} ${SIZE[size]}`}
       onClick={onClick}
     >
       {text}
