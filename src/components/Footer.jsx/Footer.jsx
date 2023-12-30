ec894f9e6f6dd182a9309315d51edf16e3642d43
msclal <mika1shanela@gmail.com>
2023-12-30 00:04:08 -0800

    cleanup icons import statement

--- src/components/Footer.jsx/Footer.jsx
+++ src/components/Footer.jsx/Footer.jsx
@@ -1,9 +1,11 @@
 "use client";
 import React, { useState } from "react";
 import { BsGear } from "react-icons/bs";
-import { HiOutlineLocationMarker } from "react-icons/hi";
-import { HiOutlinePhone } from "react-icons/hi";
-import { HiOutlineMail } from "react-icons/hi";
+import {
+  HiOutlineLocationMarker,
+  HiOutlinePhone,
+  HiOutlineMail,
+} from "react-icons/hi";
 import Button from "../Button";
 
 const Footer = () => {
@@ -27,7 +29,7 @@ const Footer = () => {
   };
 
   return (
-    <div className="bg-parts-blue text-white h-[350px] p-14 flex justify-evenly">
+    <div className="bg-parts-blue text-white h-[350px] p-14 flex justify-evenly font-outfit">
       <div className="flex flex-col h-full justify-between">
         {/* RPARTS LOGO + CONTACT INFO */}
         <div className="space-y-2">
@@ -38,17 +40,17 @@ const Footer = () => {
           </p>
           <div className="flex items-center gap-x-5">
             <HiOutlineLocationMarker className="text-xl stroke-2" />
-            <p className="font-medium text-sm">
+            <p className="font-medium">
               900 University Ave, Riverside, CA 92521
             </p>
           </div>
           <div className="flex items-center gap-x-5">
             <HiOutlinePhone className="text-xl stroke-2" />
-            <p className="font-medium text-sm">+1 (123) 456-7890</p>
+            <p className="font-medium">+1 (123) 456-7890</p>
           </div>
           <div className="flex items-center gap-x-5">
             <HiOutlineMail className="text-xl stroke-2" />
-            <p className="font-medium text-sm">contact@rparts.com</p>
+            <p className="font-medium">contact@rparts.com</p>
           </div>
         </div>
         <p className="text-sm font-light">
@@ -59,7 +61,7 @@ const Footer = () => {
       {/* FEEDBACK INPUT TEXTAREA */}
       <div className="flex flex-col justify-between gap-y-5">
         <div>
-          <p className="text-xl font-medium">Give Us your Feedback!</p>
+          <p className="text-xl font-semibold">Give Us your Feedback!</p>
           <span className="text-sm">
             {" R'Parts"} is a growing community and we are actively looking for
             feedback from you! <br />
