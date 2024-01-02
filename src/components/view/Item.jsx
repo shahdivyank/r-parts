5505cbccdecfc23114f5c3ee09bea158226cafa1
Divyank Shah <divyank.shah.2016@gmail.com>
2024-01-01 18:06:25 -0800

    format view page

--- src/components/view/Item.jsx
+++ src/components/view/Item.jsx
@@ -9,10 +9,6 @@ const Item = () => {
         <p className="font-outfit text-parts-gray-500 mb-8">MARKET / PRODUCT</p>
         <div className="md:grid grid-cols-5 gap-8">
           <div className="col-span-3">
-            {/* 
-                    images={selectedItem.data.images}
-                    first={selectedItem.data.images[0]} 
-                    */}
             <ViewImages images={null} first={null} />
           </div>
           <div className="col-span-2">
---
+++ src/components/view/View.jsx
@@ -0,0 +1,11 @@
+import Item from "./Item";
+
+const View = () => {
+  return (
+    <div>
+      <Item />
+    </div>
+  );
+};
+
+export default View;
