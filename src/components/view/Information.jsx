f159320553e4d75f49c72e5f88767503ab108eca
Zorten <zergionaruto@hotmail.com>
2024-01-01 20:04:13 -0800

    began creating table for product info

--- src/components/view/Information.jsx
+++ src/components/view/Information.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const Information = () => {
-  return <div>Information</div>;
+const Information = ({ productInfo }) => {
+  return (
+    <div className="container mx-auto p-4">
+      <table className="table-auto w-full border-collapse border">
+        <thead>
+          <tr>
+            <th className="text-left font-bold py-2">Product Dimensions</th>
+            <th className="text-left font-bold py-2">Item Weight</th>
+            {/* Add remaining headers for other product information */}
+          </tr>
+        </thead>
+        <tbody>
+          {productInfo.map((item) => (
+            <tr key={item.label}>
+              <td className="text-left py-1 px-2">{item.label}</td>
+              <td className="text-left py-1 px-2">{item.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Information;
--- src/components/view/View.jsx
+++ src/components/view/View.jsx
@@ -1,11 +1,17 @@
 import Information from "./Information";
 import Item from "./Item";
 
+const productInfo = [
+  { label: "Product Dimensions", value: "10 x 5 x 2 inches" },
+  { label: "Item Weight", value: "1 pound" },
+  // ... other product information items
+];
+
 const View = () => {
   return (
     <div className="w-11/12">
       <Item />
-      <Information />
+      <Information productInfo={productInfo} />
     </div>
   );
 };
