0d1a59a41f6f7036fc70c5183ca45f3721e17e87
Rodrigo <rddlamas@gmail.com>
2024-01-04 12:19:36 -0800

    Buttons for nav on profile are fully implemented according to Figma.

--- src/components/profile/Navigation.jsx
+++ src/components/profile/Navigation.jsx
@@ -10,14 +10,14 @@ const Navigation = () => {
   };
 
   return (
-    <div className="flex font-outfit">
+    <div className="flex font-outfit space-x-2 pt-5">
       <button
         onClick={() => handleButtonClick("Orders")}
         className={`${
           activeButton === "Orders"
             ? "bg-parts-orange text-white"
             : "bg-transparent border border-gray-300 text-gray-400"
-        }  px-3 py-1 mr-1 focus:outline-none rounded border-2 duration-300 ease-in-out hover:bg-parts-orange hover:text-white`}
+        }  px-4 py-3 mr-1 h-fit focus:outline-none rounded-lg border-2 duration-300 ease-in-out hover:bg-parts-orange hover:text-white`}
       >
         ORDERS
       </button>
@@ -27,7 +27,7 @@ const Navigation = () => {
           activeButton === "Listed Items"
             ? "bg-parts-orange text-white"
             : "bg-transparent border-2 border-gray-300 text-gray-400"
-        } px-3 py-1 mr-1 focus:outline-none flex-shrink-0 rounded border-2 transition duration-300 ease-in-out hover:bg-parts-orange hover:text-white`}
+        } px-4 py-3 mr-1 h-fit focus:outline-none flex-shrink-0 rounded-lg border-2 transition duration-300 ease-in-out hover:bg-parts-orange hover:text-white`}
       >
         LISTED ITEMS
       </button>
@@ -37,7 +37,7 @@ const Navigation = () => {
           activeButton === "Past Purchases"
             ? "bg-parts-orange text-white"
             : "bg-transparent border-2 border-gray-300 text-gray-400"
-        }  px-3 py-1 mr-1 focus:outline-none flex-shrink-0 rounded border-2 duration-300 ease-in-out hover:bg-parts-orange hover:text-white`}
+        }  px-4 py-3 mr-1 h-fit focus:outline-none flex-shrink-0 rounded-lg border-2 duration-300 ease-in-out hover:bg-parts-orange hover:text-white`}
       >
         PAST PURCHASES
       </button>
@@ -47,7 +47,7 @@ const Navigation = () => {
           activeButton === "Sold Items"
             ? "bg-parts-orange text-white"
             : "bg-transparent border-2 border-gray-300 text-gray-400"
-        }  px-3 py-1 focus:outline-none flex-shrink-0 rounded border-2 duration-300 ease-in-out hover:bg-parts-orange hover:text-white`}
+        }  px-4 py-3 h-fit focus:outline-none flex-shrink-0 rounded-lg border-2 duration-300 ease-in-out hover:bg-parts-orange hover:text-white`}
       >
         SOLD ITEMS
       </button>
