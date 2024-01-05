beaa2919f35314aaa5fb38f18a8107b2725f3642
Mariam Golwalla <mgolw001@ucr.edu>
2024-01-04 17:10:40 -0800

    search frontend

--- src/app/sell/page.js
+++ src/app/sell/page.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Search from "@/components/sell/Search";
 
 const Sell = () => {
-  return <div>Sell</div>;
+  return (
+    <div className="bg-gray-200 w-full h-full flex justify-center">
+      <div className="w-11/12">
+        {" "}
+        <Search />{" "}
+      </div>
+    </div>
+  );
 };
 
 export default Sell;
