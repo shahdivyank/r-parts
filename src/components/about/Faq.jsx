f1bce7745102b1b875fda2f8be2858217e201c02
Alamode89 <andreamante89@gmail.com>
2024-01-05 03:51:54 -0800

    Added arrow and rotates when question is clicked

--- src/components/about/Faq.jsx
+++ src/components/about/Faq.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { React, useState } from "react";
+import { FiChevronRight } from "react-icons/fi";
 
 const Faq = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -7,12 +8,15 @@ const Faq = ({ question, answer }) => {
   return (
     <>
       <div
-        className="bg-red-500 w-3/4 rounded-xl cursor-pointer"
+        className="flex flex-col w-11/12 rounded-xl cursor-pointer border-2 border-gray-400 p-3 m-3"
         onClick={() => setIsOpen(!isOpen)}
       >
-        {question}
+        <div className="flex flex-row justify-between">
+          {question}
+          <FiChevronRight className={`text-3xl ${isOpen ? "rotate-90" : ""}`} />
+        </div>
+        {isOpen && <div className="">{answer}</div>}
       </div>
-      {isOpen && <div>{answer}</div>}
     </>
   );
 };
