2192f98076ec9be367ee6aa09e92c9604a09ded7
Alamode89 <andreamante89@gmail.com>
2024-01-05 01:51:31 -0800

    Text style and added new placeholder image

--- src/components/market/Item.jsx
+++ src/components/market/Item.jsx
@@ -4,19 +4,20 @@ import PlaceHolder from "../../../public/svg/item.svg";
 
 const Item = () => {
   return (
-    <div>
+    <div className="bg-blue-500 w-1/4">
       <Image
         src={PlaceHolder}
         alt={"Item"}
-        priority={true}
         draggable={false}
-        className="object-cover rounded-xl w-1/6 h-1/"
+        width={200}
+        height={50}
+        className="rounded-xl"
       />
       {/* Title and description */}
 
       <div>
-        <p>Lorem ipsum dolor</p>
-        <p>
+        <p className="text-black font-semibold">Lorem ipsum dolor</p>
+        <p className="font-light text-sm">
           Lorem ipsum dolor sit amet, consectetur adipiscin elit. Vestibulum
           convallis
         </p>
@@ -24,10 +25,14 @@ const Item = () => {
 
       {/* Price and Condition */}
 
-      <div className="flex flex-row">
-        <span>$0.00</span>
-        <span>$0.00</span>
-        <div>Condition: New</div>
+      <div className="flex flex-row justify-between">
+        <div className="font-semibold ">
+          <span className="text-parts-gray-500 line-through">$00.00</span>
+          <span className="text-black">$00.00</span>
+        </div>
+        <p>
+          Condition: <span className="text-parts-green">New</span>
+        </p>
       </div>
     </div>
   );
--- src/components/market/Items.jsx
+++ src/components/market/Items.jsx
@@ -3,7 +3,7 @@ import Item from "./Item";
 
 const Items = () => {
   return (
-    <div className="bg-red-500 w-full">
+    <div className="w-full">
       <Item />
     </div>
   );
