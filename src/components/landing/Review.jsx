0bffcecf1bc154ba78fd84e4829ab11c097e301d
theo <theobfernandez@gmail.com>
2024-01-21 00:01:23 -0800

    revert last commit; wrong branch lol

--- src/components/landing/Review.jsx
+++ src/components/landing/Review.jsx
@@ -3,21 +3,21 @@ import profile from "../../../public/information-placeholder.webp";
 import Stars from "./Stars";
 
 const Review = () => {
-  return (
-    <div className="flex bg-white mx-3 rounded-lg p-8 font-outfit">
-      <div className="w-1/4 flex flex-col items-center justify-center">
-        <Image src={profile} className="rounded-full w-16" />
-        <p className="font-semibold">Bobby Lerias</p>
-        <p className="text-xs text-parts-gray-500 font-light">CUSTOMER</p>
-        <Stars rating={4} />
-      </div>
-      <div className="w-3/4 p-4 font-light">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit.
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex bg-white rounded-lg p-8 font-outfit">
+			<div className="w-1/4 flex flex-col items-center justify-center">
+				<Image src={profile} className="rounded-full w-16" />
+				<p className="font-semibold">Bobby Lerias</p>
+				<p className="text-xs text-parts-gray-500 font-light">CUSTOMER</p>
+				<Stars rating={4} />
+			</div>
+			<div className="w-3/4 p-4 font-light">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor
+				sit amet, consectetur adipiscing elit.
+			</div>
+		</div>
+	);
 };
 
 export default Review;
