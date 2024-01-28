b6382bd8ac780b1684e4236cec141c617b9af327
shahdivyank <divyank.shah.2016@gmail.com>
2024-01-27 20:21:06 -0800

    simplify component

--- src/components/view/Information.jsx
+++ src/components/view/Information.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Information = ({
   dimensions,
   weight,
@@ -9,25 +7,12 @@ const Information = ({
   totalPrice,
 }) => {
   const renderField = (label, value) => {
-    if (value == null || value == undefined) {
-      return (
-        <>
-          <div className="bg-gray-200 pb-2 px-8 py-2 font-semibold">
-            {label}
-          </div>
-          <div className="px-8 py-2">Not Available</div>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <div className="bg-gray-200 pb-2 px-8 py-2 font-semibold">
-            {label}
-          </div>
-          <div className="px-8 py-2">{value}</div>
-        </>
-      );
-    }
+    return (
+      <>
+        <div className="bg-gray-200 pb-2 px-8 py-2 font-semibold">{label}</div>
+        <div className="px-8 py-2">{value ?? "Not Available"}</div>
+      </>
+    );
   };
   return (
     <div className="grid grid-cols-2 w-6/12 justify-start divide-y divide-gray-400 font-outfit mb-8">
