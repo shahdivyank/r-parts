47bdc99ffcfbd7fce7b6f95666e7d80d0ff278b8
Zorten <zergionaruto@hotmail.com>
2024-02-05 18:51:47 -0800

    Created components for Info page. Added them to Info.jsx component since that where FAQ was. All items are on left, I tried centering but no success

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,14 +1,20 @@
 import FAQ from "./Faq";
 import Questions from "@/data/Questions.js";
+import WhatIs from "@/components/info/WhatIs.jsx";
+import HowItWorks from "@/components/info/HowItWorks.jsx";
 
 const Info = () => {
   return (
-    <div className="w-1/2 rounded-xl border border-gray-300 px-5 py-5 m-10">
-      <p className="text-lg font-semibold">FAQs</p>
-      <div className="border-b border-gray-300 my-2" />
-      {Questions.map(({ index, question, answer }) => (
-        <FAQ key={index} question={question} answer={answer} />
-      ))}
+    <div>
+      <WhatIs />
+      <HowItWorks />
+      <div className="w-1/2 rounded-xl border border-gray-300 px-5 py-5 m-10">
+        <p className="text-lg font-semibold">FAQs</p>
+        <div className="border-b border-gray-300 my-2" />
+        {Questions.map(({ index, question, answer }) => (
+          <FAQ key={index} question={question} answer={answer} />
+        ))}
+      </div>
     </div>
   );
 };
