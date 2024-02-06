613a78aceb5402d85084c9029eb5bcae33406a09
msclal <mika1shanela@gmail.com>
2024-02-05 19:17:10 -0800

    finalize banners

--- src/components/landing/Banner.jsx
+++ src/components/landing/Banner.jsx
@@ -1,14 +1,14 @@
 const Banner = () => {
-  const years = 100;
-  const parts = "3M";
-  const professors = "5M";
+  const years = 5;
+  const parts = 10;
+  const professors = 120;
 
   return (
     <div className="w-full flex justify-evenly py-7 bg-parts-navy text-white font-outfit my-10">
       <div className="flex items-center">
         <p className="font-bold text-5xl">{years}+</p>
         <div className="ml-2 text-sm font-light">
-          YEARS <br /> OF SERVICE
+          SATISFIED <br /> CUSTOMERS
         </div>
       </div>
       <div className="flex items-center">
@@ -18,9 +18,9 @@ const Banner = () => {
         </div>
       </div>
       <div className="flex items-center">
-        <p className="font-bold text-5xl">{professors}+</p>
+        <p className="font-bold text-5xl">${professors}+</p>
         <div className="ml-2 text-sm font-light">
-          ACTIVE <br /> PROFESSORS
+          TOTAL CUSTOMER <br /> DOLLARS SAVED
         </div>
       </div>
     </div>
