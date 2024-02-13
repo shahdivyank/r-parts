ef0067276772487b3e1ad8ee2684868e57871afb
Mariam Golwalla <mgolw001@ucr.edu>
2024-02-12 18:09:38 -0800

    button styling for white + removed placehold bg and text

--- src/data/Button.js
+++ src/data/Button.js
@@ -3,6 +3,8 @@ export const COLOR = {
     "bg-parts-orange border-parts-orange text-white hover:opacity-70",
   "border-orange":
     "bg-transparent border-parts-orange text-parts-orange border-2 hover:bg-parts-orange hover:opacity-30 hover:text-white",
+  white:
+    "bg-parts-gray-100 text-parts-orange hover:bg-parts-orange hover:text-white",
 };
 export const SIZE = {
   sm: "px-10 py-2 text-xs sm:text-sm",
