997bc1df7b79aaf52ba87ae2673c07c81a22cad4
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2024-02-19 19:08:18 -0800

    cleaned banner/header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,17 @@ const Header = ({ title }) => {
   return (
     <div className="w-full overflow-hidden flex flex-col items-end justify-center h-[80px] md:h-[120px] lg:h-[190px]">
       {Array.from({ length: 3 }).map((_, row) => (
-        <div key={row} className="text-4xl md:text-6xl lg:text-8xl font-bebas">
+        <div
+          key={row}
+          className="!leading-[5rem] text-4xl md:text-6xl lg:text-8xl font-bebas"
+        >
           {Array.from({ length: 10 }, (_, col) => (
             <span
               key={col}
               className={`${
                 row === 1 && col === 9
                   ? "text-parts-orange"
-                  : "text-parts-gray-200"
+                  : "text-parts-gray-300"
               }`}
             >
               {title}
