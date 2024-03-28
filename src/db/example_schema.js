32e74e2a545cf5cd436e36d31cb26c18e228671c
Kobe Braga <kbrag003@ucr.edu>
2024-03-27 22:22:29 -0700

    added drizzle config and example schema file

---
+++ src/db/example_schema.js
@@ -0,0 +1,11 @@
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const users = sqliteTable('users', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull(),
+  email: text('email').unique().notNull(),
+});
+
+export type InsertUser = typeof users.$inferInsert;
+export type SelectUser = typeof users.$inferSelect;
